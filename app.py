0ef02d990e15c1581b402139e0ca8206dfc372e4
Alexis Costedoat <67351701+costeva@users.noreply.github.com>
2024-11-08 17:13:54 -0300

    Actualizaciones SQL

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mutant_records.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class DNARecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dna_sequence = db.Column(db.String, unique=True, nullable=False)
+    is_mutant = db.Column(db.Boolean, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 def is_mutant(dna):
     n = len(dna)
@@ -15,7 +31,7 @@ def is_mutant(dna):
                 return False
         return True
 
-    # Buscar secuencias horizontales, verticales y diagonales
+  
     for i in range(n):
         for j in range(n):
             if j + 3 < n and check_sequence(i, j, 0, 1):
@@ -31,19 +47,55 @@ def is_mutant(dna):
                 return True
 
     return False
-
 @app.route('/')
 def home():
     return "Bienvenido a la API de Mutant Detector. Usa el endpoint /mutant para verificar ADN.", 200
-
+    
 @app.route('/mutant', methods=['POST'])
 def mutant():
     data = request.get_json()
     dna = data.get('dna', [])
-    if is_mutant(dna):
+    if not dna:
+        return "Bad Request: Missing 'dna' parameter", 400
+
+    
+    dna_sequence_str = ','.join(dna)
+
+    record = DNARecord.query.filter_by(dna_sequence=dna_sequence_str).first()
+    if record:
+        is_mutant_result = record.is_mutant
+    else:
+   
+        is_mutant_result = is_mutant(dna)
+       
+        new_record = DNARecord(dna_sequence=dna_sequence_str, is_mutant=is_mutant_result)
+        db.session.add(new_record)
+        db.session.commit()
+
+
+    if is_mutant_result:
         return "Mutant detected", 200
     else:
         return "Forbidden", 403
+
+@app.route('/stats', methods=['GET'])
+def stats():
+  
+    count_mutant_dna = DNARecord.query.filter_by(is_mutant=True).count()
+    count_human_dna = DNARecord.query.filter_by(is_mutant=False).count()
+
+
+    if (count_human_dna + count_mutant_dna) > 0:
+        ratio = count_mutant_dna / (count_human_dna + count_mutant_dna)
+    else:
+        ratio = 0
+
+ 
+    return jsonify({
+        "count_mutant_dna": count_mutant_dna,
+        "count_human_dna": count_human_dna,
+        "ratio": ratio
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
